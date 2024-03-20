89db1ed6562853b74edb2f253026f0bad8dd180e
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-03-20 13:02:50 +0100

    Made progress main

--- Program.cs
+++ Program.cs
@@ -1,42 +1,14 @@
 
 ﻿namespace RPGWithXML
 {
-    class Program
+    public class Program
     {
-        static void Main()
+        public static void Main()
         {
-            string path = "";
-            using StreamReader sr = new StreamReader(path);
-            string xml;
-            List <Character> allCharacters = new List<Character>();
-            while ((xml = sr.ReadLine()) != null)
-            {
-                Character character = xml.Deserialize<Character>();
-                Console.WriteLine(character.ToString());
-                Console.WriteLine("Para cambiar estas caracteristicas pulsa 1, para seguir pulsa cualquier otra tecla");
-                if (Console.ReadLine() == "1")
-                {
-                    Console.WriteLine("Introduce el nuevo nombre");
-                    character.Name = Console.ReadLine();
-                    Console.WriteLine("Introduce el nuevo nivel");
-                    character.Level = Convert.ToUInt32(Console.ReadLine());
-                    Console.WriteLine("Introduce el nuevo ataque");
-                    character.Attack = Convert.ToUInt32(Console.ReadLine());
-                    Console.WriteLine("Introduce la nueva defensa");
-                    character.Defense = Convert.ToUInt32(Console.ReadLine());
-                    Console.WriteLine("Introduce la nueva vida");
-                    character.MaxHP = Convert.ToUInt32(Console.ReadLine());
-                }
-                allCharacters.Add(character);
-                
-            }
-            using StreamWriter sw = new StreamWriter(path)
-            {
-                foreach (Character character in allCharacters)
-                {
-                    sw.WriteLine(character.Serialize());
-                }
-            }
+            const string AskCharName = "Escribe el nombre del personaje";
+            const string AskCharAttack = "Escribe el ataque del personaje";
+            const string AskCharDefense = "Escribe la defensa del personaje";
+            const string AskChar
         }
     }
 }

04e05e35a8a2d3f9acc48b7f2fc5bd0fbe9dc477
argo <argo>
2024-03-20 12:00:24 +0100

    implemented Class Battle, with static function "Attack Damage"

---
+++ Battle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGWithXML
+{
+    public class Battle
+    {
+        public static uint AttackDamage(Character player, Character rival)
+        {
+            return player.Attack - rival.Defense;
+        }
+    }
+}

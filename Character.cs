5068b8d94d74dd6569eb927e5b986404fe0e8f8d
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-03-20 11:05:09 +0100

    Made constructor

--- Character.cs
+++ Character.cs
@@ -2,9 +2,22 @@
 {
     public class Character
     {
+        const uint StartLevel = 0;
         public string Name { get; set; }
         public uint Level { get; set; }
         public uint Attack { get; set; }
         public uint Defense { get; set; }
+
+        public Character(string name, uint level,uint attack, uint defense)
+        {
+            this.Name = name;
+            this.Level = level;
+            this.Attack = attack;
+            this.Defense = defense;
+        }
+        public Character(string name, uint attack, uint defense):this(name,StartLevel,attack,defense) 
+        {
+        
+        }
     }
 }

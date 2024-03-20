1e6d195c64e3207b86714a5651c94c3b43bf6b7b
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-03-20 10:56:25 +0100

    Made Character with basic elements

---
+++ Character.cs
@@ -0,0 +1,10 @@
+﻿namespace RPGWithXML
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public uint Level { get; set; }
+        public uint Attack { get; set; }
+        public uint Defense { get; set; }
+    }
+}

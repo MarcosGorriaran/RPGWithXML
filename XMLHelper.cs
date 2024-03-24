c0dce9c66ecadeedd632afd4c550363fded53e11
Marcos <marcos.gorriaran.7e7@itb.cat>
2024-03-25 00:09:43 +0100

    Changed Serialization process, now the Character object handles the serialization while XMLHelper handles the CRUD process

--- XMLHelper.cs
+++ XMLHelper.cs
@@ -12,65 +12,22 @@ namespace RPGWithXML
         const string CharAttack = "attack";
         const string Defense = "defense";
 
-        //USING LINQ TO XML: CREATE XML FILE
-        public static void CreateXMLFile(string filePath, string name, uint level, int health, uint attack, uint defense)
-        {
-            XDocument xmlDoc = new XDocument(
-                new XElement(CharactersName,
-                    new XElement(CharacterClassName,
-                        new XElement(CharName, name),
-                        new XElement(CharLevel, level),
-                        new XElement(CharHP, health),
-                        new XElement(CharAttack, attack),
-                        new XElement(Defense, defense)
-                    )
-                )
-            );
-
-            //filePath => "direccion path";
-            xmlDoc.Save(filePath);
-
-            
-        }
-
-        //USING LINQ TO XML: ADD NEW CHARACTER TO XML FILE
         public static void AddCharacterToXMLFile(string filePath, string name, uint level, int health, uint attack, uint defense)
         {
-            XDocument xmlDoc = XDocument.Load(filePath);
-
-            XElement newCharacter = new XElement(CharacterClassName,
-                                            new XElement(CharName, name),
-                                            new XElement(CharLevel, level),
-                                            new XElement(CharHP, health),
-                                            new XElement(CharAttack, attack),
-                                            new XElement(Defense, defense)
-                                        );
-
-            xmlDoc.Root.Add(newCharacter);
-
-            xmlDoc.Save(filePath);
-
+            List<Character> characters;
+            try
+            {
+                characters = Character.XMLDeserializeGroup(filePath);
+            }
+            catch (FileNotFoundException)
+            {
+                characters = new List<Character>();
+            }
             
-        }
+            characters.Add(new Character(name,level,health,attack,defense));
 
-        //USING LINQ TO XML: READ XML FILE
-        public static List<Character> ReadXMLFile(string filePath)
-        {
-            XDocument xmlDoc = XDocument.Load(filePath);
-
-            //Lee todos los elementos "character" dentro de "characters" y los convierte en objetos Character
-            var characters = from character in xmlDoc.Descendants(CharacterClassName)
-                             select new Character
-                             {
-                                 Name = character.Element(CharName).Value,
-                                 Level = Convert.ToUInt32(character.Element(CharLevel).Value),
-                                 Health = Convert.ToInt32(character.Element(CharHP).Value),
-                                 Attack = Convert.ToUInt32(character.Element(CharAttack).Value),
-                                 Defense = Convert.ToUInt32(character.Element(Defense).Value)
-                             };
-
-            //"characters" es de tipo IEnumerable<T>, con .ToList() lo convertimos a List<Character>
-            return characters.ToList(); 
+            Character.XMLSerialize(filePath, characters.ToArray());
+            
         }
 
         public static string PrintResult(List<Character> characters)
@@ -86,40 +43,39 @@ namespace RPGWithXML
         //Lee un personaje en especifico, por el nombre (Este metodo lo usaremos para el combate)
         public static Character SelectCharacter(string filePath, string specificName)
         {
-            XDocument xmlDoc = XDocument.Load(filePath);
+            List<Character> xmlDoc = Character.XMLDeserializeGroup(filePath);
 
-            var character = (from selection in xmlDoc.Descendants(CharacterClassName)
-                             where selection.Element(CharName).Value == specificName
-                             select new Character
-                             {
-                                 Name = selection.Element(CharName).Value,
-                                 Level = Convert.ToUInt32(selection.Element(CharLevel).Value),
-                                 Health = Convert.ToInt32(selection.Element(CharHP).Value),
-                                 Attack = Convert.ToUInt32(selection.Element(CharAttack).Value),
-                                 Defense = Convert.ToUInt32(selection.Element(Defense).Value)
-                             }).SingleOrDefault(); ;
+            var character = (from selection in xmlDoc
+                             where selection.Name == specificName
+                             select selection);
 
-            return character;
+            return character.First<Character>();
         }
-
 
         //USING LINQ TO XML: UPDATE XML FILE
         public static void UpdateXMLFile (string filePath,string specificName, string name, uint level, int health, uint attack, uint defense)
         {
-            XDocument xmlDoc = XDocument.Load(filePath);
+            List<Character> characters = Character.XMLDeserializeGroup(filePath);
+            bool found=false;
 
-            var character = xmlDoc.Descendants(CharacterClassName).FirstOrDefault(
-                c => c.Element(CharName)?.Value == specificName);
-
-            character.Element(CharName).Value = name;
-            character.Element(CharLevel).Value = level.ToString();
-            character.Element(CharHP).Value = health.ToString();
-            character.Element(CharAttack).Value = attack.ToString();
-            character.Element(Defense).Value = defense.ToString();
-
-            xmlDoc.Save(filePath);
-
+            for (int i = 0; i < characters.Count; i++) 
+            {
+                if (characters[i].Name == specificName)
+                {
+                    characters[i].Name = name;
+                    characters[i].Level = level;
+                    characters[i].Health = health;
+                    characters[i].Attack = attack;
+                    characters[i].Defense = defense;
+                    found = true;
+                }
+            }
             
+            if (!found)
+            {
+                throw new Exception();
+            }
+            Character.XMLSerialize(filePath, characters.ToArray());
         }
     }
 }
